295654368c8a482efecc4025396714889dcb8271
Urielin <urieljnvp@gmail.com>
2018-01-19 09:42:12 -0500

    corrigiendo errores con el registro

--- landingpage_mpt/conexion.php
+++ landingpage_mpt/conexion.php
@@ -1,25 +1,28 @@
 <?php
-	
+
 	/*$mysqli=new mysqli("localhost","root","password","excel"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	
+
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
 	}*/
 $serverName = "10.10.1.55"; //serverName\instanceName
 $connectionInfo = array( "Database"=>"BDCONCURSOAPP", "UID"=>"sa", "PWD"=>"qwerty.123");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+// $conn = sqlsvr_connect( $serverName, $connectionInfo);
 
+$conn = mssql_connect($serverName, 'sa', 'qwerty.123');
+$conndb = mssql_select_db('BDCONCURSOAPP', $conn);
+var_dump($conndb);
 if( $conn ) {
      echo "Conexión establecida.<br />";
 }else{
      echo "Conexión no se pudo establecer.<br />";
-     die( print_r( sqlsrv_errors(), true));
-}	
+     die( print_r( sqlsvr_errors(), true));
+}
 
 /*$servidor = "10.10.1.55";
 $database = "excel";
 $info = array('Database'=>$database);
-$cn = sqlsrv_connect($servidor, $info);*/
+// $cn = sqlsvr_connect($servidor, $info);*/
 
 ?>
--- landingpage_mpt/leer.php
+++ landingpage_mpt/leer.php
@@ -1,8 +1,11 @@
     <?php
     $serverName = "10.10.1.55"; //serverName\instanceName
     $connectionInfo = array( "Database"=>"BDCONCURSOAPP", "UID"=>"sa", "PWD"=>"qwerty.123");
-    $conn = sqlsrv_connect( $serverName, $connectionInfo);
-    	
+    // $conn = sqlsvr_connect( $serverName, $connectionInfo);
+    // $conn = sqlsvr_connect( $serverName, $connectionInfo);
+    $conn = mssql_connect($serverName, 'sa', 'qwerty.123');
+    $conndb = mssql_select_db('BDCONCURSOAPP', $conn);
+    // var_dump($conndb);
     		$nombre = $_POST['nombre'];
     		$apellido = $_POST['apellido'];
     		$dni = $_POST['dni'];
@@ -10,29 +13,43 @@
     		$celular = $_POST['celular'];
     		$email = $_POST['email'];
     		$titulo = $_POST['titulo'];
-    		$descripcion = $_POST['descripcion'];    		
-    		    		
+    		$descripcion = $_POST['descripcion'];
+
     		$sql = "
-    				insert into 
+    				insert into
     					dbo.inscripcion_concurso
     					  (
     						  nombre, apellido, dni, ciudad, celular, email, titulo, descripcion
-    						  
+
     					  )
     				values
     					  (
-    						  '$nombre','$apellido','$dni','$ciudad','$celular','$email','$titulo','$descripcion'			  						  	  				  			 
-    						  
-    					  )  
-    		";
+    						  '$nombre','$apellido','$dni','$ciudad','$celular','$email','$titulo','$descripcion'
 
-    		$inscripcion_concurso=sqlsrv_prepare($conn,$sql);		
-    		sqlsrv_execute($inscripcion_concurso);
-    		
+    					  )
+    		";
+        // $sql = "
+    		// 		insert into
+    		// 			dbo.inscripcion_concurso
+    		// 			  (
+    		// 				  nombre, apellido, dni, ciudad, celular, email, titulo, descripcion
+        //
+    		// 			  )
+    		// 		values
+    		// 			  (
+    		// 				  'nombre','apellido','71206269','ciudad','celular','email','titulo','descripcion'
+    		// 			  )
+    		// ";
+        $result=mssql_query($sql);
+        // var_dump($result);
+        mssql_close();
+    		// $inscripcion_concurso=sqlsvr_prepare($conn,$sql);
+    		// sqlsvr_execute($inscripcion_concurso);
+
     		/*alert('registrado correctamente');*/
 
-    		echo "<script type='text/javascript'>alert('registrado correctamente');</script>";
-    		header("location:redireccion.html");
-    		
-    ?>
+    		// echo "<script type='text/javascript'>alert('registrado correctamente');</script>";
+        print "<meta http-equiv=\"refresh\" content=\"0;URL='./'\">";
+    		// header("location:redireccion.html");
 
+    ?>
